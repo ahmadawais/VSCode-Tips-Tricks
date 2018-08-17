77e7ad01a940f1af076490225ff01e0aa44d587d
Ahmad Awais <me@AhmadAwais.com>
2018-08-17 20:38:26 +0500

    📦 NEW: Chrome Debugging React App

--- videos/JavaScript-Debug/3-chrome/src/App.js
+++ videos/JavaScript-Debug/3-chrome/src/App.js
@@ -22,12 +22,9 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
+					<h1 className="App-title">VSCode.pro: Chrome + React App Frontend Debugging! FTW!</h1>
 				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-				<button onClick={this.handleClick}>{this.state.isToggleOn ? 'ON' : 'OFF'}</button>
+				<button onClick={this.handleClick}>{this.state.isToggleOn ? 'OFF' : 'ON'}</button>
 			</div>
 		);
 	}
