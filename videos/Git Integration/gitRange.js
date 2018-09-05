90734df61ffabaf65fdd5aee7f5fedc8e0a914aa
Ahmad Awais <me@AhmadAwais.com>
2018-09-05 09:58:52 +0500

    📦 NEW: Git Stage/Commit Selected Ranges

--- videos/Git Integration/gitRange.js
+++ videos/Git Integration/gitRange.js
@@ -3,19 +3,3 @@
  *
  * @package VSCode.pro
  */
-
-/**
- * Summation.
- *
- * @param {Number} a
- * @param {Number} b
- */
-const sum = (a, b) => a + b;
-
-/**
- * Subtraction.
- *
- * @param {Number} a
- * @param {Number} b
- */
-const sub = (a, b) => a - b;
